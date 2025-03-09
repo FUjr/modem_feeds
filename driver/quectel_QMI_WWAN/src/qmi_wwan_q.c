a4619fa1ec9b2b2025798fb3c051dc89faed8781
sfwtw <wtw@cr.cx>
2025-03-09 22:54:29 +0800

    fix: qmi_wwan_* for higher kernel versions
    Signed-off-by: sfwtw <wtw@cr.cx>

--- driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
+++ driver/quectel_QMI_WWAN/src/qmi_wwan_q.c
@@ -828,35 +828,35 @@ static struct rtnl_link_stats64 *_rmnet_vnd_get_stats64(struct net_device *net, 
 #if (LINUX_VERSION_CODE < KERNEL_VERSION( 6,1,0 ))
 		u64 rx_packets, rx_bytes;
 		u64 tx_packets, tx_bytes;
+#else
+		u64_stats_t rx_packets, rx_bytes;
+		u64_stats_t tx_packets, tx_bytes;
+#endif
 
 		stats64 = per_cpu_ptr(dev->stats64, cpu);
 
 		do {
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION( 6,6,0 ))
+			start = u64_stats_fetch_begin(&stats64->syncp);
+#else
 			start = u64_stats_fetch_begin_irq(&stats64->syncp);
+#endif
 			rx_packets = stats64->rx_packets;
 			rx_bytes = stats64->rx_bytes;
 			tx_packets = stats64->tx_packets;
 			tx_bytes = stats64->tx_bytes;
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION( 6,6,0 ))
+		} while (u64_stats_fetch_retry(&stats64->syncp, start));
+#else
 		} while (u64_stats_fetch_retry_irq(&stats64->syncp, start));
+#endif
 
+#if (LINUX_VERSION_CODE < KERNEL_VERSION( 6,1,0 ))
 		stats->rx_packets += rx_packets;
 		stats->rx_bytes += rx_bytes;
 		stats->tx_packets += tx_packets;
 		stats->tx_bytes += tx_bytes;
 #else
-        u64_stats_t rx_packets, rx_bytes;
-		u64_stats_t tx_packets, tx_bytes;
-
-		stats64 = per_cpu_ptr(dev->stats64, cpu);
-
-		do {
-			start = u64_stats_fetch_begin_irq(&stats64->syncp);
-			rx_packets = stats64->rx_packets;
-			rx_bytes = stats64->rx_bytes;
-			tx_packets = stats64->tx_packets;
-			tx_bytes = stats64->tx_bytes;
-		} while (u64_stats_fetch_retry_irq(&stats64->syncp, start));
-
         stats->rx_packets += u64_stats_read(&rx_packets);
 		stats->rx_bytes += u64_stats_read(&rx_bytes);
 		stats->tx_packets += u64_stats_read(&tx_packets);
